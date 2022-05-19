95b9345643cc65ac1f42603e9f3692e69ae3023e
Ed Page <eopage@gmail.com>
2022-05-19 10:19:57 -0500

    docs(anstyle): Expand the intro

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,27 @@
 //! ANSI Text Styling
 //!
-//! The core type is [`Style`].
+//! `anstyle` provides core types describing [ANSI styling escape
+//! codes](https://en.wikipedia.org/wiki/ANSI_escape_code) for interoperability
+//! between crates.
+//!
+//! Example use cases:
+//! - An argument parser allowing callers to define the colors used in the help-output without
+//!   putting the text formatting crate in the public API
+//! - A style description parser that can work with any text formatting crate
+//!
+//! Priorities:
+//! 1. API stability
+//! 2. Low compile-time and binary-size overhead
+//! 3. `const` friendly API for callers to statically define their stylesheet
+//!
+//! For integration with text styling crate, see:
+//! - [anstyle-termcolor](https:://docs.rs/anstyle-termcolor)
+//! - [anstyle-owo-colors](https:://docs.rs/anstyle-owo-colors)
+//! - [anstyle-yansi](https:://docs.rs/anstyle-yansi)
 //!
 //! # Examples
 //!
+//! The core type is [`Style`]:
 //! ```rust
 //! let style = anstyle::Style::new().bold();
 //! ```
