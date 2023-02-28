d5705e89a7a34624b86cfa32ec8e24d1d9c4640a
Ed Page <eopage@gmail.com>
2023-02-28 07:18:55 -0600

    test(parse): Switch benches to criterion

--- crates/anstyle-parse/benches/parse.rs
+++ crates/anstyle-parse/benches/parse.rs
@@ -1,49 +1,42 @@
-#![cfg_attr(all(feature = "nightly", test), feature(test))]
+use criterion::{black_box, Criterion};
 
-#[cfg(all(feature = "nightly", test))]
-mod bench {
-    extern crate std;
-    extern crate test;
+use anstyle_parse::*;
 
-    use anstyle_parse::*;
+static VTE_DEMO: &[u8] = include_bytes!("../tests/demo.vte");
 
-    use test::{black_box, Bencher};
-
-    static VTE_DEMO: &[u8] = include_bytes!("../tests/demo.vte");
-
-    struct BenchDispatcher;
-    impl Perform for BenchDispatcher {
-        fn print(&mut self, c: char) {
-            black_box(c);
-        }
-
-        fn execute(&mut self, byte: u8) {
-            black_box(byte);
-        }
-
-        fn hook(&mut self, params: &Params, intermediates: &[u8], ignore: bool, c: char) {
-            black_box((params, intermediates, ignore, c));
-        }
-
-        fn put(&mut self, byte: u8) {
-            black_box(byte);
-        }
-
-        fn osc_dispatch(&mut self, params: &[&[u8]], bell_terminated: bool) {
-            black_box((params, bell_terminated));
-        }
-
-        fn csi_dispatch(&mut self, params: &Params, intermediates: &[u8], ignore: bool, c: char) {
-            black_box((params, intermediates, ignore, c));
-        }
-
-        fn esc_dispatch(&mut self, intermediates: &[u8], ignore: bool, byte: u8) {
-            black_box((intermediates, ignore, byte));
-        }
+struct BenchDispatcher;
+impl Perform for BenchDispatcher {
+    fn print(&mut self, c: char) {
+        black_box(c);
     }
 
-    #[bench]
-    fn testfile(b: &mut Bencher) {
+    fn execute(&mut self, byte: u8) {
+        black_box(byte);
+    }
+
+    fn hook(&mut self, params: &Params, intermediates: &[u8], ignore: bool, c: char) {
+        black_box((params, intermediates, ignore, c));
+    }
+
+    fn put(&mut self, byte: u8) {
+        black_box(byte);
+    }
+
+    fn osc_dispatch(&mut self, params: &[&[u8]], bell_terminated: bool) {
+        black_box((params, bell_terminated));
+    }
+
+    fn csi_dispatch(&mut self, params: &Params, intermediates: &[u8], ignore: bool, c: char) {
+        black_box((params, intermediates, ignore, c));
+    }
+
+    fn esc_dispatch(&mut self, intermediates: &[u8], ignore: bool, byte: u8) {
+        black_box((intermediates, ignore, byte));
+    }
+}
+
+fn testfile(c: &mut Criterion) {
+    c.bench_function("testfile", |b| {
         b.iter(|| {
             let mut dispatcher = BenchDispatcher;
             let mut parser = Parser::new();
@@ -51,12 +44,13 @@ mod bench {
             for byte in VTE_DEMO {
                 parser.advance(&mut dispatcher, *byte);
             }
-        });
-    }
+        })
+    });
+}
 
-    #[bench]
-    fn state_changes(b: &mut Bencher) {
-        let input = b"\x1b]2;X\x1b\\ \x1b[0m \x1bP0@\x1b\\";
+fn state_changes(c: &mut Criterion) {
+    let input = b"\x1b]2;X\x1b\\ \x1b[0m \x1bP0@\x1b\\";
+    c.bench_function("state_changes", |b| {
         b.iter(|| {
             let mut dispatcher = BenchDispatcher;
             let mut parser = Parser::new();
@@ -66,6 +60,9 @@ mod bench {
                     parser.advance(&mut dispatcher, *byte);
                 }
             }
-        });
-    }
+        })
+    });
 }
+
+criterion::criterion_group!(benches, testfile, state_changes);
+criterion::criterion_main!(benches);
