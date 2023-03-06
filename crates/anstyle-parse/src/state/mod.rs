7d4a1d8649bacc0468fb6f348539c356fb4b6aa0
Ed Page <eopage@gmail.com>
2023-03-05 22:00:09 -0600

    refactor(parse): Pull out table

--- crates/anstyle-parse/src/state/mod.rs
+++ crates/anstyle-parse/src/state/mod.rs
@@ -8,4 +8,11 @@ pub(crate) use definitions::pack;
 pub(crate) use definitions::unpack;
 pub(crate) use definitions::Action;
 pub(crate) use definitions::State;
-pub(crate) use table::state_change;
+
+#[inline]
+pub(crate) const fn state_change(state: State, byte: u8) -> u8 {
+    let state_idx = state as usize;
+    let byte_idx = byte as usize;
+
+    table::STATE_CHANGES[state_idx][byte_idx]
+}
