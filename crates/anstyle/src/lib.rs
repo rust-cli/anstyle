7de27f37776a2ac08a3d676b95c3780b88a11a85
Ed Page <eopage@gmail.com>
2023-03-06 03:55:22 -0600

    docs(anstyle): Fix links

--- crates/anstyle/src/lib.rs
+++ crates/anstyle/src/lib.rs
@@ -15,16 +15,17 @@
 //! 3. `const` friendly API for callers to statically define their stylesheet
 //!
 //! For integration with text styling crate, see:
-//! - [anstyle-ansi-term](https:://docs.rs/anstyle-ansi-term)
-//! - [anstyle-crossterm](https:://docs.rs/anstyle-crossterm)
-//! - [anstyle-termcolor](https:://docs.rs/anstyle-termcolor)
-//! - [anstyle-owo-colors](https:://docs.rs/anstyle-owo-colors)
-//! - [anstyle-yansi](https:://docs.rs/anstyle-yansi)
+//! - [anstyle-ansi-term](https://docs.rs/anstyle-ansi-term)
+//! - [anstyle-crossterm](https://docs.rs/anstyle-crossterm)
+//! - [anstyle-termcolor](https://docs.rs/anstyle-termcolor)
+//! - [anstyle-owo-colors](https://docs.rs/anstyle-owo-colors)
+//! - [anstyle-yansi](https://docs.rs/anstyle-yansi)
 //!
 //! General utilities:
-//! - [anstyle-git](https:://docs.s/anstyle-git): Parse Git style descriptions
-//! - [anstyle-ls](https:://docs.s/anstyle-ls): Parse LS_COLORS style descriptions
-//! - [anstyle-lossy](https:://docs.s/anstyle-lossy): Convert between `anstyle::Color` types
+//! - [anstyle-git](https://docs.s/anstyle-git): Parse Git style descriptions
+//! - [anstyle-ls](https://docs.s/anstyle-ls): Parse LS_COLORS style descriptions
+//! - [anstyle-lossy](https://docs.s/anstyle-lossy): Convert between `anstyle::Color` types
+//! - [anstyle-roff](https://docs.s/anstyle-roff): For converting to ROFF
 //!
 //! # Examples
 //!
