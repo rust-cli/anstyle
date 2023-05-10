7f3624d4a6108acf2134e12d936c8b7884e9d1fe
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2023-05-10 21:01:56 +0100

    refactor: Re-use clap's colorchoice in colorchoice_clap

--- crates/colorchoice-clap/src/lib.rs
+++ crates/colorchoice-clap/src/lib.rs
@@ -11,6 +11,8 @@
 //! }
 //! ```
 
+pub use clap::ColorChoice;
+
 /// Mixin a clap argument for colored output selection
 #[derive(Copy, Clone, Default, Debug, PartialEq, Eq, clap::Args)]
 #[command(about = None, long_about = None)]
@@ -35,66 +37,6 @@ impl Color {
         }
     }
 }
-
-/// Argument value for when to color output
-#[derive(Copy, Clone, Debug, PartialEq, Eq)]
-pub enum ColorChoice {
-    Auto,
-    Always,
-    Never,
-}
-
-impl ColorChoice {
-    /// Report all `possible_values`
-    pub fn possible_values() -> impl Iterator<Item = clap::builder::PossibleValue> {
-        use clap::ValueEnum;
-        Self::value_variants()
-            .iter()
-            .filter_map(clap::ValueEnum::to_possible_value)
-    }
-
-    fn as_str(&self) -> &'static str {
-        match self {
-            Self::Auto => AUTO,
-            Self::Always => ALWAYS,
-            Self::Never => NEVER,
-        }
-    }
-}
-
-impl Default for ColorChoice {
-    fn default() -> Self {
-        Self::Auto
-    }
-}
-
-impl std::fmt::Display for ColorChoice {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        self.as_str().fmt(f)
-    }
-}
-
-impl std::str::FromStr for ColorChoice {
-    type Err = String;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        <Self as clap::ValueEnum>::from_str(s, false)
-    }
-}
-
-impl clap::ValueEnum for ColorChoice {
-    fn value_variants<'a>() -> &'a [Self] {
-        &[Self::Auto, Self::Always, Self::Never]
-    }
-
-    fn to_possible_value(&self) -> Option<clap::builder::PossibleValue> {
-        Some(clap::builder::PossibleValue::new(self.as_str()))
-    }
-}
-
-const AUTO: &str = "auto";
-const ALWAYS: &str = "always";
-const NEVER: &str = "never";
 
 #[cfg(test)]
 mod test {
