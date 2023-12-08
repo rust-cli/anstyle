1b0b59a3918ee6beb05be29f9912ec8ffa8e38d9
Ed Page <eopage@gmail.com>
2023-12-08 10:20:56 -0600

    feat(query): Expose wincon errors

--- crates/anstyle-query/src/windows.rs
+++ crates/anstyle-query/src/windows.rs
@@ -32,7 +32,7 @@ mod windows_console {
         }
     }
 
-    fn enable_ansi_colors_raw() -> std::io::Result<()> {
+    pub fn enable_virtual_terminal_processing() -> std::io::Result<()> {
         let stdout = std::io::stdout();
         let stdout_handle = stdout.as_raw_handle();
         let stderr = std::io::stderr();
@@ -48,7 +48,11 @@ mod windows_console {
 
     #[inline]
     pub(crate) fn enable_ansi_colors() -> Option<bool> {
-        Some(enable_ansi_colors_raw().map(|_| true).unwrap_or(false))
+        Some(
+            enable_virtual_terminal_processing()
+                .map(|_| true)
+                .unwrap_or(false),
+        )
     }
 }
 
@@ -65,4 +69,10 @@ mod windows_console {
 /// For non-windows systems, returns `None`
 pub fn enable_ansi_colors() -> Option<bool> {
     windows_console::enable_ansi_colors()
+}
+
+/// Raw ENABLE_VIRTUAL_TERMINAL_PROCESSING on stdout/stderr
+#[cfg(windows)]
+pub fn enable_virtual_terminal_processing() -> std::io::Result<()> {
+    windows_console::enable_virtual_terminal_processing()
 }
