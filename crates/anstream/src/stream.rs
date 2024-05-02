255c79bbd72108929acf9d8a07dfad23f3227489
Ed Page <eopage@gmail.com>
2024-05-02 13:03:13 -0500

    fix: Drop MSRV to 1.64
    To avoid Divan having a higher MSRV, we'll skip checking
    dev-dependencies

--- crates/anstream/src/stream.rs
+++ crates/anstream/src/stream.rs
@@ -50,14 +50,14 @@ pub trait IsTerminal: private::Sealed {
 impl IsTerminal for std::io::Stdout {
     #[inline]
     fn is_terminal(&self) -> bool {
-        std::io::IsTerminal::is_terminal(self)
+        is_terminal_polyfill::IsTerminal::is_terminal(self)
     }
 }
 
 impl IsTerminal for std::io::StdoutLock<'_> {
     #[inline]
     fn is_terminal(&self) -> bool {
-        std::io::IsTerminal::is_terminal(self)
+        is_terminal_polyfill::IsTerminal::is_terminal(self)
     }
 }
 
@@ -71,14 +71,14 @@ impl IsTerminal for &'_ mut std::io::StdoutLock<'_> {
 impl IsTerminal for std::io::Stderr {
     #[inline]
     fn is_terminal(&self) -> bool {
-        std::io::IsTerminal::is_terminal(self)
+        is_terminal_polyfill::IsTerminal::is_terminal(self)
     }
 }
 
 impl IsTerminal for std::io::StderrLock<'_> {
     #[inline]
     fn is_terminal(&self) -> bool {
-        std::io::IsTerminal::is_terminal(self)
+        is_terminal_polyfill::IsTerminal::is_terminal(self)
     }
 }
 
@@ -120,7 +120,7 @@ impl IsTerminal for &'_ mut Vec<u8> {
 impl IsTerminal for std::fs::File {
     #[inline]
     fn is_terminal(&self) -> bool {
-        std::io::IsTerminal::is_terminal(self)
+        is_terminal_polyfill::IsTerminal::is_terminal(self)
     }
 }
 
