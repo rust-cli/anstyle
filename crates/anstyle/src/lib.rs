a41b13a27ff6cf49fb083cd72c2d9c2ffdfdf35d
Ed Page <eopage@gmail.com>
2025-04-17 11:32:57 -0500

    chore: Catch no_std problems earlier

--- crates/anstyle/src/lib.rs
+++ crates/anstyle/src/lib.rs
@@ -47,6 +47,8 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 #![cfg_attr(docsrs, feature(doc_auto_cfg))]
 #![warn(missing_docs)]
+#![warn(clippy::std_instead_of_core)]
+#![warn(clippy::std_instead_of_alloc)]
 #![warn(clippy::print_stderr)]
 #![warn(clippy::print_stdout)]
 
