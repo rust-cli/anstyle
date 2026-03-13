9f2df7276c1de35ad0a1e685bf74728b44701696
Ed Page <eopage@gmail.com>
2026-03-13 16:39:41 -0500

    feat(progress): Add skeleton

---
+++ crates/anstyle-progress/CHANGELOG.md
@@ -0,0 +1,13 @@
+# Change Log
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/)
+and this project adheres to [Semantic Versioning](https://semver.org/).
+
+<!-- next-header -->
+## [Unreleased] - ReleaseDate
+
+Initial release
+
+<!-- next-url -->
+[Unreleased]: https://github.com/rust-cli/anstyle/compare/8ed060819922c65d8bdf37bb83442db64579c53f...HEAD
---
+++ crates/anstyle-progress/src/lib.rs
@@ -0,0 +1,16 @@
+//! ANSI escape code progress reporting (OSC 9;4)
+//!
+//! For details on the protocol, see
+//! [ConEmu's docs](https://conemu.github.io/en/AnsiEscapeCodes.html#ConEmu_specific_OSC)
+
+#![cfg_attr(not(feature = "std"), no_std)]
+#![cfg_attr(docsrs, feature(doc_cfg))]
+#![warn(missing_docs)]
+#![warn(clippy::std_instead_of_core)]
+#![warn(clippy::std_instead_of_alloc)]
+#![warn(clippy::print_stderr)]
+#![warn(clippy::print_stdout)]
+
+#[doc = include_str!("../README.md")]
+#[cfg(doctest)]
+pub struct ReadmeDoctests;
