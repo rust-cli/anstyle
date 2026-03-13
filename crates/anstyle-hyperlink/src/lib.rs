b700d181e17cedf7085fef38f926d092af1099d8
Ed Page <eopage@gmail.com>
2026-03-13 09:41:01 -0500

    feat(hyperlink): Add skeleton

---
+++ crates/anstyle-hyperlink/src/lib.rs
@@ -0,0 +1,13 @@
+//! ANSI escape code hyperlink
+
+#![cfg_attr(not(feature = "std"), no_std)]
+#![cfg_attr(docsrs, feature(doc_cfg))]
+#![warn(missing_docs)]
+#![warn(clippy::std_instead_of_core)]
+#![warn(clippy::std_instead_of_alloc)]
+#![warn(clippy::print_stderr)]
+#![warn(clippy::print_stdout)]
+
+#[doc = include_str!("../README.md")]
+#[cfg(doctest)]
+pub struct ReadmeDoctests;
