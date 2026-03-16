cfc550add6ac7e1947a9edcb3e781908dfd1f99f
Ed Page <eopage@gmail.com>
2026-03-16 12:21:38 -0500

    refactor(progress): Switch query to fallback scheme

--- crates/anstyle-progress/src/query.rs
+++ crates/anstyle-progress/src/query.rs
@@ -1,10 +1,22 @@
 /// Determines whether the terminal supports ANSI OSC 9;4.
 pub fn supports_term_progress(is_terminal: bool) -> bool {
-    let windows_terminal = std::env::var("WT_SESSION").is_ok();
-    let conemu = std::env::var("ConEmuANSI").ok() == Some("ON".into());
+    if !is_terminal {
+        return false;
+    }
+
+    if std::env::var("WT_SESSION").is_ok() {
+        return true;
+    }
+
+    if std::env::var("ConEmuANSI").ok() == Some("ON".into()) {
+        return true;
+    }
+
     let term_program = std::env::var("TERM_PROGRAM").ok();
-    let wezterm = term_program == Some("WezTerm".into());
-    let ghostty = term_program == Some("ghostty".into());
+    if matches!(term_program.as_deref(), Some("WezTerm") | Some("ghostty")) {
+        return true;
+    }
+
     // iTerm added OSC 9;4 support in v3.6.6, which we can check for.
     // For context: https://github.com/rust-lang/cargo/pull/16506#discussion_r2706584034
     let iterm = term_program == Some("iTerm.app".into())
@@ -12,6 +24,10 @@ pub fn supports_term_progress(is_terminal: bool) -> bool {
             .ok()
             .map(|v| term_features_has_progress(&v))
             .unwrap_or(false);
+    if iterm {
+        return true;
+    }
+
     // Ptyxis added OSC 9;4 support in 48.0.
     // See https://gitlab.gnome.org/chergert/ptyxis/-/issues/305
     let ptyxis = std::env::var("PTYXIS_VERSION")
@@ -19,8 +35,11 @@ pub fn supports_term_progress(is_terminal: bool) -> bool {
         .and_then(|version| version.split(".").next()?.parse::<i32>().ok())
         .map(|major_version| major_version >= 48)
         .unwrap_or(false);
+    if ptyxis {
+        return true;
+    }
 
-    (windows_terminal || conemu || wezterm || ghostty || iterm || ptyxis) && is_terminal
+    false
 }
 
 // For iTerm, the TERM_FEATURES value "P" indicates OSC 9;4 support.
